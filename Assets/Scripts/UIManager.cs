a38d951cf10e99d123b209922971663297e956e2
Alec Arme <Incubusalec@gmail.com>
2015-11-12 14:27:21 -0800

    Add basic start scene and UI. Currently working with mouse

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public void loadLevel(string name) {
+		Application.LoadLevel (name);
+	}
+
+	public void quit() {
+		Application.Quit ();
+	}
+	
+}
